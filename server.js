0fd2c0215dd750c1130bf8d55f2b380ac2198728
haki-user <adityapratapsingh33@outlook.com>
2023-02-11 10:02:16 +0530

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,71 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const exphbs = require('express-handlebars');
+
+// const app = express();
+
+// // Connect to MongoDB
+// mongoose.connect('mongodb://localhost/placement', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+// // Load the Student model
+// const Student = require('./models/Student');
+
+// // Use body-parser middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Use handlebars as the templating engine
+// app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+// // GET / - Show all student records
+// app.get('/', (req, res) => {
+//   Student.find({}, (err, students) => {
+//     if (err) throw err;
+//     res.render('index', { data: students });
+//   });
+// });
+
+// // GET /analysis - Show analysis of student records
+// app.get('/analysis', (req, res) => {
+//   Student.aggregate([
+//     {
+//       $group: {
+//         _id: '$department',
+//         averagePackage: { $avg: '$packageOffered' }
+//       }
+//     }
+//   ], (err, result) => {
+//     if (err) throw err;
+//     res.render('analysis', { data: result });
+//   });
+// });
+
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+const PORT = 5000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res)=>{
+  res.render(path.join(__dirname+"./index.html"))
+})
+
+app.listen(PORT, ()=>{
+  console.log(`ok port ${PORT}`)
+})
